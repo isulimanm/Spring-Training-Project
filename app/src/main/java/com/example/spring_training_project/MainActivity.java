46944cd8fc016d4ddd8d23999faf93f34ad0095e
isulimanm <isulimanm@gmail.com>
2025-01-16 03:31:36 +0300

    sprint 1.1

--- app/src/main/java/com/example/spring_training_project/MainActivity.java
+++ app/src/main/java/com/example/spring_training_project/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.spring_training_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,22 +20,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private Button continueB;
+
+    private ImageView logo;
+    private TextView appName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        continueB = findViewById(R.id.continueB);
+        logo = findViewById(R.id.logo);
+        appName = findViewById(R.id.appName);
+
+        AlphaAnimation fade = new AlphaAnimation(0f, 1f);
+        fade.setDuration(4000);
+        fade.setFillAfter(true);
+
+        TranslateAnimation moveUp = new TranslateAnimation(0, 0, 4000, 0);
+        moveUp.setDuration(4000);
+        moveUp.setFillAfter(true);
+
+        AnimationSet all = new AnimationSet(true);
+        all.addAnimation(fade);
+        all.addAnimation(moveUp);
 
 
-        continueB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,SecActivity.class);
-                startActivity(intent);
-            }
-        });
+        logo.startAnimation(all);
+        appName.startAnimation(all);
+
+        logo.startAnimation(fade);
+        appName.startAnimation(fade);
+
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, SecActivity.class));
+        }, 4000);
 
 
     }
