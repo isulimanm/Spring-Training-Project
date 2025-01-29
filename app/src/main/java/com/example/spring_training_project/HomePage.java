ea9a366a7198d8aaff7d881d05c9cac110f42b65
isulimanm <isulimanm@gmail.com>
2025-01-29 21:29:47 +0300

    scrollable list with button

--- app/src/main/java/com/example/spring_training_project/HomePage.java
+++ app/src/main/java/com/example/spring_training_project/HomePage.java
@@ -9,7 +9,7 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sec);
+        setContentView(R.layout.activity_home);
 
     }
 }
